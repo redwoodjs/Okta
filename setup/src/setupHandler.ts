5b5035dbb5da4b60152937207a9f87ef7654f503
Tobbe Lundberg <tobbe@tlundberg.com>
2023-01-27 19:46:16 +0100

    setupHandler: Format source

--- setup/src/setupHandler.ts
+++ setup/src/setupHandler.ts
@@ -5,22 +5,17 @@ import { standardAuthHandler } from '@redwoodjs/cli-helpers'
 
 import { Args } from './setup'
 
-const { version } = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json'), 'utf-8'))
+const { version } = JSON.parse(
+  fs.readFileSync(path.join(__dirname, '../package.json'), 'utf-8')
+)
 
 export const handler = async ({ force: forceArg }: Args) => {
   standardAuthHandler({
     basedir: __dirname,
     forceArg,
     provider: 'okta',
-    authDecoderImport:
-      "import { authDecoder } from '@redwoodjs/auth-okta-api'",
-    webPackages: [
-      '@okta/okta-auth-js',
-      `@redwoodjs/auth-okta-web@${version}`,
-    ],
-    apiPackages: [
-      '@okta/jwt-verifier',
-      `@redwoodjs/auth-okta-api@${version}`,
-    ],
+    authDecoderImport: "import { authDecoder } from '@redwoodjs/auth-okta-api'",
+    webPackages: ['@okta/okta-auth-js', `@redwoodjs/auth-okta-web@${version}`],
+    apiPackages: ['@okta/jwt-verifier', `@redwoodjs/auth-okta-api@${version}`],
   })
 }
